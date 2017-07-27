8a58ddbdd46957252aa362d25527780c02022a3f
No0ne <github@exa.solar>
2017-07-27 20:11:14 +0200

    Update sms.php

--- sms.php
+++ sms.php
@@ -3,11 +3,14 @@
 $log = date('Y-m-d H:i:s').PHP_EOL;
 $file = 'sms.log';
 
-foreach (explode(';',$_GET['rec']) as $rec) {
-        $log.= (sprintf('gammu sendsms TEXT %s -text "%s"', $rec,str_replace('"',"'",$_GET['text'])));
-        $log.= PHP_EOL;
-        $log.= shell_exec(sprintf('gammu sendsms TEXT %s -text "%s"', $rec,str_replace('"',"'",$_GET['text'])));
-        $log.= PHP_EOL;
-        $log.= PHP_EOL;
+foreach(explode(';', $_GET['rec']) as $rec) {
+        $rec = filter_var($rec, FILTER_SANITIZE_NUMBER_INT);
+        
+        if(!empty($rec)) {
+                $cmd = sprintf('gammu sendsms TEXT %s -textutf8 %s', $rec, escapeshellarg($_GET['text']));
+                $log.= $cmd.PHP_EOL;
+                $log.= shell_exec($cmd).PHP_EOL.PHP_EOL;
+        }
 }
 file_put_contents($file, $log, FILE_APPEND | LOCK_EX);
+echo $log;
